386acacff095de2e56430c34cd4253666c374bd1
faritg1 <113639494+faritg1@users.noreply.github.com>
2023-10-03 12:08:47 -0500

    feat: :hammer: Agregando las entidades en la clase de contexto

--- Infrastructure/Data/AnimalsContext.cs
+++ Infrastructure/Data/AnimalsContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
@@ -12,6 +13,15 @@ public class AnimalsContext : DbContext
     {
     }
 
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Pais> Paises { get; set; }
+    public DbSet<Departamento> Departamentos { get; set; }
+    public DbSet<Cita> Citas { get; set; }
+    public DbSet<Mascota> Mascotas { get; set; }
+    public DbSet<Raza> Razas { get; set; }
+    public DbSet<Servicio> Servicios { get; set; }
+    public DbSet<ClienteDireccion> ClientesDirecciones { get; set; }
+    public DbSet<ClienteTelefono> ClientesTelefonos { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
